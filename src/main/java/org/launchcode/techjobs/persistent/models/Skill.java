89220d4807797086c7e896952c1c7dfd73555a7a
jojotier <122423763+jojotier@users.noreply.github.com>
2023-11-30 16:38:54 -0600

    Half of task 4 completed- just need to update the Home and List controllers

--- src/main/java/org/launchcode/techjobs/persistent/models/Skill.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Skill.java
@@ -1,15 +1,29 @@
 package org.launchcode.techjobs.persistent.models;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToMany;
 import jakarta.validation.constraints.Size;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class Skill extends AbstractEntity {
 
+    @ManyToMany(mappedBy = "skills")
+    private List<Job> jobs = new ArrayList<Job>();
     @Size(max=500)
     private String description;
 
     public Skill(){};
+
+    public List<Job> getJobs() {
+        return jobs;
+    }
+
+    public void setJobs(List<Job> jobs) {
+        this.jobs = jobs;
+    }
 
     public String getDescription() {
         return description;
